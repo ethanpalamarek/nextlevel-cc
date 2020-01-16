ceef143be20393319fb45468e3249d8dbade2a3c
Ethan Palamarek <ethanpala39@gmail.com>
2020-01-15 22:45:15 -0700

    Update site-config.js

--- site-config.js
+++ site-config.js
@@ -1,15 +1,20 @@
 const path = require('path');
 
 module.exports = {
-  siteTitle: `Gatsby Universal`,
-  siteTitleShort: `GatsbyU`,
-  siteDescription: `An opinionated starter for Gatsby.`,
-  siteUrl: `https://gu.fabianschultz.com`,
+  siteTitle: `Manteo Cycling`,
+  siteTitleShort: `Manteo`,
+  siteDescription: `A fresh perspective`,
+  siteUrl: `https://manteocycling.ca`,
   themeColor: `#000`,
   backgroundColor: `#fff`,
   pathPrefix: null,
   logo: path.resolve(__dirname, 'src/images/icon.png'),
+<<<<<<< Updated upstream
   social: {
     fbAppId: `966242223397117`,
+=======
+  social: { 
+    fbAppId: `104362544393047`,
+>>>>>>> Stashed changes
   },
 };
