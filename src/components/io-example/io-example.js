4e294ae39c1bc215633b606add4b4be9f6df6dbd
Ethan Palamarek <ethanpala39@gmail.com>
2020-01-15 22:22:33 -0700

    Update io-example.js

--- src/components/io-example/io-example.js
+++ src/components/io-example/io-example.js
@@ -11,7 +11,7 @@ const IOExample = () => (
   <IO rootMargin="-50px">
     {({ isVisible }) => (
       <Container isVisible={isVisible}>
-        <Title tag="span">IntersectionObserver</Title>
+        <Title tag="span">Next Level-cc 2020</Title>
       </Container>
     )}
   </IO>
